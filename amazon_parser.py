fcaaf043f7a4da8c2e4ff17c1441ce88d8c1d5ec
Ritesh Shrivastav <ritesh_shrv@live.com>
2015-10-30 16:44:00 +0530

    Improves amazon_parser script

--- amazon_parser.py
+++ amazon_parser.py
@@ -2,22 +2,37 @@ import urllib.request
 import json
 from bs4 import BeautifulSoup, SoupStrainer
 
-to_crawl=[]
-crawled=[]
-l='http://www.amazon.com'
-to_crawl.append(s)
-h=urllib.request.urlopen(l)
-crawled.append(s)
 
-alinks=BeautifulSoup(h, parse_only=SoupStrainer('a', href=True))
-for tags in alinks:
-	if tags.has_attr('href'):
-		li=tags['href']
-		if li.find('http://www.amazon.com')==0 and li not in crawled:
-			to_crawl.append(li)
+def crawler():
+    '''Function that crawls entire website starting from the categories page.
+       Function creates a list of links from all the pages it had crawled.'''
+    
+    to_crawl = []
+    crawled = []
+    # amazon category page
+    l = 'http://www.amazon.in/gp/site-directory/ref=nav_shopall_btn'
+    to_crawl.append(s)
+    h = urllib.request.urlopen(l)
+    crawled.append(s)
 
-# product=BeautifulSoup(h, parse_only=SoupStrainer('title'))
-name=soup.title.contents.split(':')
-product=name[0]
-category=name[-1]
+    soup = BeautifulSoup(h)
+    for links in soup.fnd_all('a', href=True):
+        if links.get('class', 'nav_a'):
+            li = 'http://www.amazon.com' + links
+            if li not in crawled:
+                to_crawl.append(li)
+    return to_crawl
 
+
+def find_product_info(url=None):
+    '''Function to find product information viz. name, price, description, image_path.
+            Functi'''
+    
+    if url is None:
+        mi_url = 'http://www.amazon.in/gp/product/B00VEB0F22?redirect=true&ref_=s9_acss_bw_sc_MiStore_ah_s1'
+    soup = BeautifulSoup(urllib.request.urlopen(url))
+
+    name = soup.find(id="productTtile").contents
+    price = soup.find(id="priceblock_ourprice").get_text()
+    description = soup.find(id="feature-bullets")
+    img_path = soup.find(id="landingImage")
